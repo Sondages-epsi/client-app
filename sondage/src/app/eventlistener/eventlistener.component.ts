6ba1a4355dbbf31aaddc874749a47f86f73bee58
Hugo <ledall.hugo.pro@gmail.com>
2019-02-06 16:45:34 +0100

    adding buttons

--- sondage/src/app/eventlistener/eventlistener.component.ts
+++ sondage/src/app/eventlistener/eventlistener.component.ts
@@ -11,6 +11,34 @@ export class EventlistenerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+	/*const button1 = new Gpio(6, 'in', 'both');	  
+	const button2 = new Gpio(13, 'in', 'both');
+	const button3 = new Gpio(19, 'in', 'both');
+	button1.watch((err, value) => {
+		if (err) {
+			throw err;
+		}
+		sendKey(1);
+	});	
+	  
+	button2.watch((err, value) => {
+		if (err) {
+			throw err;
+		}
+		sendKey(2);
+	});	
+	  
+	button3.watch((err, value) => {
+		if (err) {
+			throw err;
+		}
+		sendKey(3);
+	});*/
   }
+  /*
+  function sendKey(number){
+	  //Implement action
+	  return number;
+  }*/
 
 }
